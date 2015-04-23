c5034d4a9737d3c42b3cd3d80f46c107e260273c
alreyes1 <alreyes1@icloud.com>
2015-04-22 22:12:29 -0400

    fix action mailer host address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'polar-thicket-1447.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'polar-thicket-1447.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
